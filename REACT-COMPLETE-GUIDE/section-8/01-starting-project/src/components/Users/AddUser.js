c28c786cd6257cbe2c54b77dd62d57b0ea7cd8d9
Hyejin Lee <hichjay@gmail.com>
2021-07-02 23:56:00 +0900

    React 강의 수강중

---
+++ REACT-COMPLETE-GUIDE/section-8/01-starting-project/src/components/Users/AddUser.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const AddUser = (props) => {
+  return (
+    <form>
+      <label htmlFor="username">Username</label>
+      <input id="username" type="text" />
+      <label htmlFor="username">Age (Years)</label>
+      <input id="username" type="text" />
+    </form>
+  );
+};
