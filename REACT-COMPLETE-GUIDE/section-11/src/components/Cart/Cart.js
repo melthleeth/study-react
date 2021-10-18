d2c3e48ef0cf3f7e74b0dd5c42224073c5f421ed
Hyejin Lee <hichjay@gmail.com>
2021-10-18 23:43:49 +0900

    Section 11: Practice Project: Building a Food Order App
    - wrap up the things learned previously
    - use React hooks (useState, useEffect, useContext)
    - create context and use it on cart

---
+++ REACT-COMPLETE-GUIDE/section-11/src/components/Cart/Cart.js
@@ -0,0 +1,56 @@
+import { useContext } from "react";
+
+import Modal from "../UI/Modal";
+import CartItem from "./CartItem";
+import classes from "./Cart.module.css";
+import CartContext from "../../store/cart-context";
+
+const Cart = (props) => {
+  const cartCtx = useContext(CartContext);
+
+  const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
+  const hasItems = cartCtx.items.length > 0;
+
+  const cartItemAddHandler = (item) => {
+    cartCtx.addItem({ ...item, amount: 1 });
+  };
+  const cartItemRemoveHandler = (id) => {
+    cartCtx.removeItem(id);
+  };
+
+  // bind를 쓴게 callback 함수 쓴거랑 똑같음
+  // <button onClick={() => this.handleClick(id)} />
+  // <button onClick={this.handleClick.bind(this, id)} />
+
+  const cartItems = (
+    <ul className={classes["cart-items"]}>
+      {cartCtx.items.map((item) => (
+        <CartItem
+          key={item.id}
+          item={item}
+          onAdd={cartItemAddHandler.bind(null, item)}
+          onRemove={cartItemRemoveHandler.bind(null, item.id)}
+        >
+          {item.name}
+        </CartItem>
+      ))}
+    </ul>
+  );
+  return (
+    <Modal onClose={props.onClose}>
+      {cartItems}
+      <div className={classes.total}>
+        <div>Total Amount</div>
+        <div>{totalAmount}</div>
+      </div>
+      <div className={classes.actions}>
+        <button className={classes["button--alt"]} onClick={props.onClose}>
+          Close
+        </button>
+        {hasItems && <button className={classes.button}>Order</button>}
+      </div>
+    </Modal>
+  );
+};
+
+export default Cart;
