8409dd4e417f6032457bc8f31e2fa486c868e78e
Hyejin Lee <hichjay@gmail.com>
2021-11-01 17:32:15 +0900

    Section 13: An Alternative Way Of Building Components: Class-based Components
    - convert functional components to class-based components

--- REACT-COMPLETE-GUIDE/section-12/src/App.js
+++ REACT-COMPLETE-GUIDE/section-12/src/App.js
@@ -1,12 +1,16 @@
-import React, { useState, useCallback } from "react";
+import React, { useState, useCallback, useMemo } from "react";
 
-import DemoOutput from "./components/UI/Button/Demo/DemoOutput";
-import Button from "./components/UI/Button/Button";
 import "./App.css";
+import DemoOutput from "./components/Demo/DemoOutput";
+import DemoList from "./components/Demo/DemoList";
+import Button from "./components/UI/Button/Button";
 
 function App() {
-  const [showParagraph, setShowParagraph] = useState();
-  const [allowToggle, setAllowToggle] = useState();
+  const [showParagraph, setShowParagraph] = useState(false);
+  const [allowToggle, setAllowToggle] = useState(false);
+  const [listTitle, setListTitle] = useState("My List");
+
+  console.log("APP RUNNING");
 
   const toggleParagraphHandler = useCallback(() => {
     if (allowToggle)
@@ -17,12 +21,17 @@ function App() {
     setAllowToggle(true);
   };
 
-  console.log("APP RUNNING");
+  const changeTitleHandler = useCallback(() => {
+    setListTitle("New Title");
+  }, []);
+
+  const listItems = useMemo(() => [5, 3, 1, 10, 9], []);
 
   return (
     <div className="app">
       <h1>Hi there!</h1>
-      {/* {showParagraph && <p>This is new!</p>} */}
+      <DemoList title={listTitle} items={listItems} />
+      <Button onClick={changeTitleHandler}>Change List Title</Button>
       <DemoOutput show={showParagraph} />
       <Button onClick={allowToggleHandler}>Allow Toggling</Button>
       <Button onClick={toggleParagraphHandler}>Show Paragraph!</Button>
