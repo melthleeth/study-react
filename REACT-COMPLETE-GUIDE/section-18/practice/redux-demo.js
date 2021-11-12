1053678f39c237b1918ddba3b2e827c5d5fcf1d8
Hyejin Lee <hichjay@gmail.com>
2021-11-12 21:53:20 +0900

    Section 18: Diving into Redux (An alternative to the Context API)
    - Redux concepts
    - use createStore to create store (1st version)
    - use Redux Toolkit (createSlice, configureStore) (2nd version)
    - split store codes

---
+++ REACT-COMPLETE-GUIDE/section-18/practice/redux-demo.js
@@ -0,0 +1,34 @@
+const redux = require("redux");
+
+const counterReducer = (state = { counter: 0 }, action) => {
+  if (action.type === "increment")
+    return {
+      counter: state.counter + 1,
+    };
+
+  if (action.type === "decrement")
+    return {
+      counter: state.counter - 1,
+    };
+
+  return state;
+};
+
+const store = redux.createStore(counterReducer);
+
+console.log("latest state", store.getState());
+
+const counterSubscriber = () => {
+  const latestState = store.getState(); // get latest snapshot of state
+  console.log("latestState", latestState);
+};
+
+store.subscribe(counterSubscriber);
+
+store.dispatch({
+  type: "increment",
+});
+
+store.dispatch({
+  type: "decrement",
+});
