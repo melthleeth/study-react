85e2c182c5a90ebf8d168957fe7a2267142de904
Hyejin Lee <hichjay@gmail.com>
2021-11-13 02:26:02 +0900

    Section 19: Advanced Redux
     - handle side-effects, async task
     - use thunk concept (return not itself but another function that returns actions)
     - Redux DevTools (Chrome extension)

---
+++ REACT-COMPLETE-GUIDE/section-19/src/store/cart-actions.js
@@ -0,0 +1,84 @@
+import { uiActions } from "./ui-slice";
+import { cartActions } from "./cart-slice";
+
+// 여긴 전부 thunk를 사용한 redux pattern
+
+export const fetchCartData = () => {
+  return async (dispatch) => {
+    const fetchData = async () => {
+      const response = await fetch(
+        `${process.env.REACT_APP_FIREBASE_URL}/cart.json`
+      );
+
+      if (!response.ok) throw new Error("Could not fetch cart data!");
+
+      return response.json();
+    };
+
+    try {
+      const cartData = await fetchData();
+      dispatch(
+        cartActions.replaceCart({
+          items: cartData.items || [],
+          totalQuantity: cartData.totalQuantity,
+        })
+      );
+    } catch (error) {
+      dispatch(
+        uiActions.showNotification({
+          status: "error",
+          title: "Error!",
+          message: "Fetching cart data failed!",
+        })
+      );
+    }
+  };
+};
+
+export const sendCartData = (cart) => {
+  return async (dispatch) => {
+    dispatch(
+      uiActions.showNotification({
+        status: "pending",
+        title: "Sending...",
+        message: "Sending cart data!",
+      })
+    );
+
+    const sendRequest = async () => {
+      const response = await fetch(
+        `${process.env.REACT_APP_FIREBASE_URL}/cart.json`,
+        {
+          method: "PUT",
+          body: JSON.stringify({
+            items: cart.items,
+            totalQuantity: cart.totalQuantity,
+          }),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error("Sending cart data failed.");
+      }
+    };
+
+    try {
+      await sendRequest();
+      dispatch(
+        uiActions.showNotification({
+          status: "success",
+          title: "Success!",
+          message: "Sent cart data successfully!!",
+        })
+      );
+    } catch (error) {
+      dispatch(
+        uiActions.showNotification({
+          status: "error",
+          title: "Error!",
+          message: "Sending cart data failed!",
+        })
+      );
+    }
+  };
+};
