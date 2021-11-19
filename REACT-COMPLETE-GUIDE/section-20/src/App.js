4902e94f67f9f925b063f0dec965030a93d4de88
Hyejin Lee <hichjay@gmail.com>
2021-11-19 23:50:06 +0900

    Section 20: Building a Multi-Page SPA with React Router (ing)
    - React Router v6 적용하여 진행중

---
+++ REACT-COMPLETE-GUIDE/section-20/src/App.js
@@ -0,0 +1,21 @@
+import { Routes, Route } from "react-router-dom";
+
+import Welcome from "./pages/Welcome";
+import Products from "./pages/Products";
+import MainHeader from "./components/MainHeader";
+
+function App() {
+  return (
+    <div>
+      <MainHeader />
+      <main>
+        <Routes>
+          <Route path="/welcome" element={<Welcome />} />
+          <Route path="/products" element={<Products />} />
+        </Routes>
+      </main>
+    </div>
+  );
+}
+
+export default App;
