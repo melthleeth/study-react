386971bdeceb92cd8f2c29186270feba6115f77e
Hyejin Lee <hichjay@gmail.com>
2021-12-16 15:45:25 +0900

    Section 26: Testing React Apps
    - how to write test code: create .test.js
    - testing tool is already set up when using create-react-app
    - write basic and simple test codes (Text match, async mock function)

---
+++ REACT-COMPLETE-GUIDE/section-26/src/components/Greeting.test.js
@@ -0,0 +1,55 @@
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import Greeting from "./Greeting";
+
+describe("Greeting component", () => {
+  test("renders Hello World as a text", () => {
+    // three As
+    // 1. Arrange
+    render(<Greeting />);
+
+    // 2. Act
+    // ... nothing
+
+    // 3. Assert
+    const helloWorldElement = screen.getByText("Hello World", { exact: false });
+    expect(helloWorldElement).toBeInTheDocument();
+  });
+
+  test(`renders "good to see you" if the button was NOT clicked`, () => {
+    render(<Greeting />);
+
+    const outputElement = screen.getByText("good to see you", {
+      exact: false,
+    });
+    expect(outputElement).toBeInTheDocument();
+  });
+
+  test('renders "Changed!" if the button was clicked', () => {
+    // Arrange
+    render(<Greeting />);
+
+    // Act
+    const buttonElement = screen.getByRole("button");
+    userEvent.click(buttonElement);
+
+    // Assert
+    const outputElement = screen.getByText("Changed!");
+    expect(outputElement).toBeInTheDocument();
+  });
+
+  test(`does not renders "good to see you" if the button was clicked`, () => {
+    // Arrange
+    render(<Greeting />);
+
+    // Act
+    const buttonElement = screen.getByRole("button");
+    userEvent.click(buttonElement);
+
+    // Assert
+    const outputElement = screen.queryByText("good to see you", {
+      exact: false,
+    });
+    expect(outputElement).toBeNull();
+  });
+});
